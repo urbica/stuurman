e4b78c6e7e135a1ab8536538219b4c9ff390026d
Сергей <sergey_gorbatyuk@MacBook-Pro-Sergej.local>
2017-04-26 18:34:44 +0300

    1.Packed response, 2.POI, green, mixed, 3.graph changed

--- functions.pyx
+++ functions.pyx
@@ -4,35 +4,51 @@ from numpy import arccos, pi
 import geopandas as gp
 from itertools import count
 from collections import deque
-import json
+from json import dumps
+
 def geocode(text):
     c = geocoder.osm(text).latlng
     coordinates = {'lat':float(c[1]),'lng':float(c[0])}
-    return json.dumps(coordinates)
+    return dumps(coordinates)
+
 def find_nearest_node(coordinates, index):
+    if type(coordinates) == str:
+        c = geocoder.yandex(coordinates).latlng
+        coordinates = [float(c[x]) for x in [1,0]]
     nearest = tuple(index.nearest(coordinates, 1))
     nearest_node = nearest[0]
     return nearest_node
-def get_path(list_of_nodes, dataset):
+
+def get_path(list_of_nodes, dataset, param = None):
+    if param is not None:
+        data = dataset[(dataset.source.isin(list_of_nodes))&(dataset.target.isin(list_of_nodes))]
+        data = data[['geometry','color_%s'%param]]
+        data.rename(columns = {'color_%s'%param:'color'}, inplace=True)
+        return data.to_json()
     data = dataset[(dataset.source.isin(list_of_nodes))&(dataset.target.isin(list_of_nodes))]
     return data.to_json()
+
 def distance(long p1, long p2, dict coords):
     cdef float x1,x2,y1,y2
     x1,y1 = coords[p1]
     x2,y2 = coords[p2]
     return (((x2-x1)**2+(y2-y1)**2)**0.5)*15
+
 def neighs_iter(key, g):
     for x in g[key].items():
         yield x
+
 def get_w(float w, float g):
-    if g>0.3:
-        return w*0.5
+    if g is not None:
+        return w*g
     else:
         return w
+
 def bidirectional_astar(G, source_coords, 
                         target_coords, heuristic,
                         spatial_index, dataset, 
-                        coords, weight = 'weight'):
+                        coords, weight = 'weight', additional_param=None):
+    
     nod = tuple([find_nearest_node(x, spatial_index) for x in [source_coords, target_coords]])
     source,target = nod
     c = [count(), count()]
@@ -41,10 +57,10 @@ def bidirectional_astar(G, source_coords,
     explored = [{}, {}]
     heu = [target,source]
     d=1
+    
     while queue[0] and queue[1]:
         d = 1-d
         _, __, v, dist, parent = heappop(queue[d])
-        
         if v in explored[1-d]:
             path1 = deque([v])
             path2 = deque([])
@@ -56,27 +72,34 @@ def bidirectional_astar(G, source_coords,
             while node2 is not None:
                 path2.append(node2)
                 node2 = explored[1-d][node2]
-            #path1.reverse()
-            path1.appendleft(source)
-            path2.append(target)
             finalpath = list(path1)+list(path2)
             return get_path(finalpath, dataset)
         
         if v in explored[d]:
             continue
-
+        
         explored[d][v] = parent
         
         for neighbor, w in neighs_iter(v, G):
-            #dl[d].append(neighbor)
             if neighbor in explored[d]:
                 continue
-            ncost = dist + get_w(w.get(weight, 1), w.get('green', 1))
+            ncost = dist + get_w(w.get(weight, 1), w.get(additional_param,1))
             if neighbor in enqueued[d]:
                 qcost, h = enqueued[d][neighbor]
                 if qcost <= ncost:
                     continue
             else:
                 h = heuristic(neighbor, heu[d], coords)
+            
             enqueued[d][neighbor] = ncost, h
             heappush(queue[d], (ncost+h, next(c[d]), neighbor, ncost, v))
+
+def composite_request(G, source_coords, target_coords, heuristic, spatial_index, dataset, coords):
+    green_route =  bidirectional_astar(G, source_coords, target_coords, 
+                                       heuristic, spatial_index, dataset, coords, additional_param = 'green')
+    poi_route = bidirectional_astar(G, source_coords, target_coords, 
+                                       heuristic, spatial_index, dataset, coords, additional_param = 'poi')
+    mixed_route = bidirectional_astar(G, source_coords, target_coords, 
+                                       heuristic, spatial_index, dataset, coords, additional_param = 'mixed')
+    answer = '[{"id":1,"type":"green","geom":%s},{"id":2,"type":"poi","geom":%s},{"id":3,"type":"composite","geom":%s}]'%(green_route,poi_route,mixed_route)
+    return answer
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import pickle
 
 from flask import Flask, request
 import networkx as nx
-from rtree import index
+from support import transform_poi, transform_green, categorialize, categorialize_poi, set_spatial_index
 import pyximport
 pyximport.install()
 
@@ -12,7 +12,7 @@ from functions import bidirectional_astar, distance, geocode
 
 path = os.path.dirname(os.path.realpath(__file__))
 
-with open(path+'/graphs/edges_osm.pickle','r') as f:
+with open(path+'/graphs/edges_plus.pickle','r') as f:
     edges = pickle.load(f)
 with open(path+'/graphs/nodes_osm.pickle','r') as f:
     nodes = pickle.load(f)
@@ -41,38 +41,27 @@ for x in range(edges.shape[0]):
 
 edges = edges.drop(edges.index[indexes])
 
+edges.poi = edges.poi.apply(transform_poi)
+edges['color_green'] = edges.green_ratio.apply(categorialize)
+edges['color_poi'] = edges.poi.apply(categorialize_poi)
+edges.green_ratio= edges.green_ratio.apply(transform_green)
+edges['mixed'] = edges.poi*edges.green_ratio
+edges['color_mixed'] = edges.mixed.apply(categorialize)
+
 for x in range(edges.shape[0]):
     a = edges.source.values[x]
     b = edges.target.values[x]
     w = edges.cost.values[x]
     g = edges.green_ratio.values[x]
-    if g>1:
-        g = 1
-    if g > 0.7:
-        g = 0.7
+    p = edges.poi.values[x]
+    gP = g*p
     G.add_node(a)
     G.add_node(b)
-    G.add_edge(a,b, {'weight':w, 'green':g})
-def set_spatial_index(coordinates):
-    p = index.Property()
-    p.dimension = 2
-    ind= index.Index(properties=p)
-    for x,y in zip(coordinates.keys(),coordinates.values()):
-        ind.add(x,y)
-    return ind
-def categorialize(x):
-    if x > 0.7:
-        return 3
-    if x > 0.3:
-        return 2
-    else:
-        return 1
+    G.add_edge(a,b, {'weight':w, 'green':g, 'poi':p, 'mixed':gP})
 
 spatial = set_spatial_index(coords)
+edges = edges[['source', 'target', 'color_green','color_poi','color_mixed','geometry']]
 G = G.adj
-
-edges['color_type'] = edges.green_ratio.apply(categorialize)
-edges = edges[['source', 'target', 'color_type','geometry']]
 
 app = Flask(__name__)
 
@@ -92,6 +81,13 @@ def index():
     page = urllib.urlopen(path+'/static/page.html').read()
     return page
 
+@app.route('/ws', methods=['POST'])
+def walking():
+    keys = request.get_json()
+    coords1 = [keys['x1'], keys['y1']]
+    coords2 = [keys['x2'], keys['y2']]
+    return composite_request(G, coords1, coords2, distance, spatial, edges, coords)
+
 @app.route('/route', methods=['POST'])
 def route():
     keys = request.get_json()
@@ -100,5 +96,5 @@ def route():
     return bidirectional_astar(G, coords1, coords2, distance, spatial, edges, coords)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, threaded=True)
     print 'Running'
