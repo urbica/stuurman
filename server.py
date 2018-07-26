e793b72f08b47ea87f7289065d17da5ab1f8d0b0
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2018-07-26 13:23:36 +0300

    disable threading

--- server.py
+++ server.py
@@ -159,5 +159,5 @@ def mighty_isochrones():
     return isochrone_from_point(coordinates, start_time, time)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
+    app.run(host='0.0.0.0')
     print 'Running'
