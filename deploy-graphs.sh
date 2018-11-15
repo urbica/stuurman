da6eb62ade7630a2d4f89a55947f39fb9607c07e
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2018-11-15 18:06:57 +0300

    cleanup

--- deploy-graphs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-scp -r graphs urbica@stuurman.urbica.co:/mnt/storage/urbica/stuurman/
-ssh urbica@stuurman.urbica.co 'bash -s' < deploy.sh
-echo 'Done'
--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-cd /mnt/storage/urbica/stuurman
-git pull
-docker-compose build
-docker-compose down
-docker-compose up -d
